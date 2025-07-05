c2cb16a0b4c3bcbf591036bda2f155d1519356af
SuperHuyGaming <minhhuyngoctruong@gmail.com>
2025-07-05 12:22:23 -0400

    push 17, update source

--- server/client/src/App/App.jsx
+++ server/client/src/App/App.jsx
@@ -1,9 +1,9 @@
-import Body from "./Body";
+import Body from "../Body";
 import Header from "./Header";
-import Jokes from "./Jokes";
-import Comments from "./Comments";
+import Jokes from "../Components/Jokes";
+import Comments from "../Comments";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SingleNewsPage from "./SingleNewsPage";
+import SingleNewsPage from "../Body/News/SingleNewsPage";
 
 export default function App() {
 	return (
--- server/client/src/Body/Body.jsx
+++ server/client/src/Body/Body.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import News from "./News.jsx";
+import News from "./News/News.jsx";
 import { Link } from "react-router-dom";
 
 function Body() {
